4109424946b4c367d097fdc3b64fb99ff8b5fc07
Hedra <tanksh24@gmail.com>
2020-04-01 17:44:42 -0400

    Fixes to remove the repeated asymmetric relationships

--- string_PPI.py
+++ string_PPI.py
@@ -29,50 +29,59 @@ def evaLink(term1, term2, predicate, link_type="ListLink",stv="", ppi=True):
     else:
         return ""
 def import_string():
-    print("********** " + str(datetime.datetime.now()))
+    print("started at " + str(datetime.datetime.now()))
     if not os.path.exists('raw_data/9606.protein.actions.v11.0.txt.gz'):
         wget.download(source,"raw_data/")
     if not os.path.exists('raw_data/human.uniprot_2_string.2018.tsv.gz'):
         wget.download(mapping,"raw_data/")
     
     df_data = pd.read_csv("raw_data/9606.protein.actions.v11.0.txt.gz", dtype=str, sep="\t")
+    df_data_symmetric = df_data[df_data['is_directional'] == "f"]
+    df_data_asymmetric = df_data[df_data['is_directional'] == "t"]
     df_mapping = pd.read_csv("raw_data/human.uniprot_2_string.2018.tsv.gz", dtype=str, sep="\t", names=["code", "uniprot", "ensembl","num1","num2"])
+   
     # create a mapping dictionary
     mapping_dict = {} 
     for e in df_mapping["ensembl"]:
         if not e in mapping_dict.keys():
             mapping_dict[e] = df_mapping[df_mapping["ensembl"] == e]["uniprot"].values[0]
-    print("Done with the Dict")
+    print("Done with the Dict, importing into atomese")
     print(len(df_data))
     notmapped = []
-    print("********** " + str(datetime.datetime.now()))
-    with open("dataset/string_ppi.scm", "w") as f, open('dataset/string_ggi.scm', 'w') as g:
-        for i in range(100):
-            try:
-                prot1 = df_data.iloc[i]['item_id_a']
-                prot2 = df_data.iloc[i]['item_id_b']
-                mode = df_data.iloc[i]['mode']
-                is_directional = df_data.iloc[i]['is_directional'] # is_directional - describes if the diractionality of the particular interaction is known.
-                a_is_acting = df_data.iloc[i]['a_is_acting'] # the directionality of the action if applicable ('t' gives that item_id_a is acting upon item_id_b)
-                score = int(df_data.iloc[i]['score'])               
-                """
-                If the directionality of the interaction is true and a is acting, use ListLink and keep the order. Otherwise use SetLink
-                Example:
-                item_id_a   item_id_b   mode    is_directional  a_is_acting
-                ENSP00000000233	ENSP00000216366 reaction    f   f   
-                <=> EvaluationLink 
-                        PredicateNode "reaction"
-                        SetLink ENSP00000000233 ENSP00000216366
 
-                ENSP00000000233	ENSP00000216366 reaction    t   f
-                <=> EvaluationLink 
-                        PredicateNode "reaction"
-                        ListLink ENSP00000216366 ENSP00000000233
-                ENSP00000000233	ENSP00000216366	reaction    t   t
-                <=> EvaluationLink 
-                        PredicateNode "reaction"
-                        ListLink ENSP00000000233 ENSP00000216366
-                """
+    """
+        If the directionality of the interaction is true and a is acting, use ListLink and keep the order. Otherwise use SetLink
+        * is_directional - describes if the diractionality of the particular interaction is known.
+        * a_is_acting - the directionality of the action if applicable ('t' gives that item_id_a is acting upon item_id_b)
+        Example:
+        item_id_a   item_id_b   mode    is_directional  a_is_acting
+        ENSP00000000233	ENSP00000216366 reaction    f   f   
+        <=> EvaluationLink 
+                PredicateNode "reaction"
+                SetLink ENSP00000000233 ENSP00000216366
+
+        ENSP00000000233	ENSP00000216366 reaction    t   f
+        <=> EvaluationLink 
+                PredicateNode "reaction"
+                ListLink ENSP00000216366 ENSP00000000233
+        ENSP00000000233	ENSP00000216366	reaction    t   t
+        <=> EvaluationLink 
+                PredicateNode "reaction"
+                ListLink ENSP00000000233 ENSP00000216366
+    
+        Keep symmetric relations and ignore if the same relation happens to be asymmetric
+    """
+    symmetric = {}
+    if not os.path.exists(os.path.join(os.getcwd(), 'string_dataset')):
+        os.makedirs('string_dataset')
+    with open("string_dataset/string_ppi_{}.scm".format(str(datetime.date.today())), "w") as f, open('string_dataset/string_ggi_{}.scm'.format(str(datetime.date.today())), 'w') as g:
+        for i in range(len(df_data_symmetric)):
+            try:
+                prot1 = df_data_symmetric.iloc[i]['item_id_a']
+                prot2 = df_data_symmetric.iloc[i]['item_id_b']
+                mode = df_data_symmetric.iloc[i]['mode']
+                score = int(df_data_symmetric.iloc[i]['score'])               
+
                 if prot1 in mapping_dict.keys() and prot2 in mapping_dict.keys():
                     prot1 = mapping_dict[prot1]
                     prot2 = mapping_dict[prot2]
@@ -87,21 +96,50 @@ def import_string():
                 gene1 = prot1.split("|")[1].split("_")[0] 
                 protein2 = prot2.split("|")[0]
                 gene2 = prot2.split("|")[1].split("_")[0]
-                if is_directional is "f":
-                   f.write(evaLink(protein1, protein2, mode, stv="(stv {} {})".format(1.0, score/1000),link_type="SetLink"))
-                   g.write(evaLink(gene1, gene2, mode,stv="(stv {} {})".format(1.0, score/1000), link_type="SetLink", ppi=False))  
+
+                f.write(evaLink(protein1, protein2, mode, stv="(stv {} {})".format(1.0, score/1000),link_type="SetLink"))
+                g.write(evaLink(gene1, gene2, mode,stv="(stv {} {})".format(1.0, score/1000), link_type="SetLink", ppi=False))  
+                symmetric[gene1 + gene2] = mode
+
+            except Exception as e:
+                print(e)
+        for i in range(len(df_data_asymmetric)):
+            try:
+                prot1 = df_data_asymmetric.iloc[i]['item_id_a']
+                prot2 = df_data_asymmetric.iloc[i]['item_id_b']
+                mode = df_data_asymmetric.iloc[i]['mode']
+                a_is_acting = df_data_asymmetric.iloc[i]['a_is_acting'] 
+                score = int(df_data_asymmetric.iloc[i]['score'])               
+
+                if prot1 in mapping_dict.keys() and prot2 in mapping_dict.keys():
+                    prot1 = mapping_dict[prot1]
+                    prot2 = mapping_dict[prot2]
                 else:
+                    if not prot1 in mapping_dict.keys():
+                        notmapped.append(prot1)
+                    else:
+                        notmapped.append(prot2)
+                    continue
+                
+                protein1 = prot1.split("|")[0]
+                gene1 = prot1.split("|")[1].split("_")[0] 
+                protein2 = prot2.split("|")[0]
+                gene2 = prot2.split("|")[1].split("_")[0]
+
+                if not (gene1+gene2 in symmetric.keys() and symmetric[gene1+gene2] == mode):
                     if a_is_acting is "t": 
                         f.write(evaLink(protein1, protein2, mode, stv="(stv {} {})".format(1.0, score/1000)))
                         g.write(evaLink(gene1, gene2, mode, stv="(stv {} {})".format(1.0, score/1000), ppi=False))
                     else:
-                        f.write(evaLink(protein2, protein1, mode, stv="(stv {} {})"))
-                        g.write(evaLink(gene2, gene1, mode, ppi=False, stv="(stv {} {})"))
+                        f.write(evaLink(protein2, protein1, mode, stv="(stv {} {})".format(1.0, score/1000)))
+                        g.write(evaLink(gene2, gene1, mode, ppi=False, stv="(stv {} {})".format(1.0, score/1000)))
 
             except Exception as e:
                 print(e)
-        print("********** " + str(datetime.datetime.now()))
-        with open("notmapped.txt", "w") as n:
+        
+        print("Done " + str(datetime.datetime.now()))
+        with open("string_dataset/notmapped_ensembles.txt", "w") as n:
             n.write("\n".join(set(notmapped)))
+
 if __name__ == "__main__":
     import_string()
