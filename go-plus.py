75009137e9c3a7ad4662069c22451c75db4af355
Hedra <tanksh24@gmail.com>
2020-04-09 09:31:44 -0400

    Editted to include chebi, uberon and CL

--- go-plus.py
+++ go-plus.py
@@ -28,27 +28,77 @@ def evaLink(term1 , term2, predicate):
     else:
         return ""
 
+def inheritLink(term1 , term2):
+    if not (str(term1) == "nan" or str(term2) == 'nan'):
+        return ("(InheritanceLink \n" +
+                "\t (ConceptNode" + " \"" + term1 + "\")\n" +
+                "\t (ConceptNode" + " \"" + term2 + "\"))\n" )
+    else:
+        return ""
+
 source = "https://bioportal.bioontology.org/ontologies/GO-PLUS"
 source_csv = "https://gitlab.com/opencog-bio/pln_mozi/blob/master/raw_data/GO-PLUS.csv.gz"
 
 if not os.path.exists("raw_data/GO-PLUS.csv.gz"):
     dataset = wget.download(source_csv, "raw_data")
 df = pd.read_csv("raw_data/GO-PLUS.csv.gz", dtype=str)
-columns = ["Class ID","Obsolete","negatively regulated by","negatively regulates", "positively regulated by", "positively regulates", "regulated by", "regulates", "has part", "part of"]
 
-df = df[columns]
+go_columns = ["negatively regulated by","negatively regulates", "positively regulated by", "positively regulates", "regulated by", "regulates", "has part", "part of"]
+uberon_columns = open("raw_data/uberon_columns.txt", "r").read().splitlines()
+cl_columns = ['has part', 'Parents', 'has role']
 
-with open("dataset/Go-Plus_{}.scm".format(str(date.today())),"w") as f:
-    for i in range(len(df)):
-        term = get_term(df.iloc[i]["Class ID"])
-        obsolete = df.iloc[i]["Obsolete"]
-        if obsolete == "false" and "GO" in term:
-            f.write(evaLink(term, get_term(df.iloc[i]["negatively regulates"]), "GO_negatively_regulates"))
-            f.write(evaLink(term, get_term(df.iloc[i]["positively regulates"]), "GO_positively_regulates"))
-            f.write(evaLink(term, get_term(df.iloc[i]["regulates"]), "GO_regulates"))
-            f.write(evaLink(term, get_term(df.iloc[i]["has part"]), "GO_has_part"))
-            f.write(evaLink(term, get_term(df.iloc[i]["part of"]), "GO_part_of"))
-            f.write(evaLink(get_term(df.iloc[i]["negatively regulated by"]), term, "GO_negatively_regulates"))
-            f.write(evaLink(get_term(df.iloc[i]["positively regulated by"]), term, "GO_positively_regulates"))
-            f.write(evaLink(get_term(df.iloc[i]["regulated by"]), term, "GO_positively_regulates"))
+if not os.path.exists("dataset/go-plus"):
+    os.mkdir("dataset/go-plus/")
+
+go = open("dataset/go-plus/Go-Plus-GO_{}.scm".format(str(date.today())),"w")
+uberon = open("dataset/go-plus/Go-Plus-UBERON_{}.scm".format(str(date.today())),"w")
+cl = open("dataset/go-plus/Go-Plus-CL_{}.scm".format(str(date.today())),"w")
+chebi = open("dataset/go-plus/Go-Plus-CHEBI_{}.scm".format(str(date.today())),"w")
+
+go_with_def = open("dataset/go-plus/Go-Plus-GO_with_definition_{}.scm".format(str(date.today())),"w")
+uberon_with_def = open("dataset/go-plus/Go-Plus-UBERON_with_definition_{}.scm".format(str(date.today())),"w")
+cl_with_def = open("dataset/go-plus/Go-Plus-CL_with_definition_{}.scm".format(str(date.today())),"w")
+chebi_with_def = open("dataset/go-plus/Go-Plus-CHEBI_with_definition_{}.scm".format(str(date.today())),"w")
+
+meta = {}
+
+print("Started importing")
+for i in range(len(df)):
+    term = get_term(df.iloc[i]["Class ID"])
+    obsolete = df.iloc[i]["Obsolete"]
+    definition = df.iloc[i]["definition"]
+    if obsolete != "true" and "GO" in term:
+        go.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        go_with_def.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        go_with_def.write(evaLink(term, definition, "GO_definition"))
+        for col in go_columns:
+            go.write(evaLink(term, get_term(df.iloc[i][col]), "GO_{}".format(col.replace(" ", "_"))))
+            go_with_def.write(evaLink(term, get_term(df.iloc[i][col]), "GO_{}".format(col.replace(" ", "_"))))
+
+    elif obsolete != "true" and "UBERON" in term:
+        uberon.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        uberon_with_def.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        uberon_with_def.write(evaLink(term, definition, "UBERON_definition"))
+        for col in uberon_columns:
+            uberon.write(evaLink(term, get_term(df.iloc[i][col]), "UBERON_{}".format(col.replace(" ", "_"))))
+
+    elif obsolete != "true" and "CL" in term or "CHEBI" in term:
+        if "CL" in term:
+            file_name = cl
+            file_name_with_def = cl_with_def
+        else:
+            file_name = chebi
+            file_name_with_def = chebi_with_def
+
+        file_name.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        file_name_with_def.write(evaLink(term, get_term(df.iloc[i]["Preferred Label"]), "has_name"))
+        file_name_with_def.write(evaLink(term, definition, "UBERON_definition"))
+        for col in cl_columns:
+            if col == "Parents":
+                file_name.write(inheritLink(term, get_term(df.iloc[i][col])))
+                file_name_with_def.write(inheritLink(term, get_term(df.iloc[i][col])))
+            else:
+                file_name.write(evaLink(term, get_term(df.iloc[i][col]), "CL_{}".format(col.replace(" ", "_"))))
+                file_name_with_def.write(evaLink(term, get_term(df.iloc[i][col]), "CL_{}".format(col.replace(" ", "_"))))
+print("Done")
             
