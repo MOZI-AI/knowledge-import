2b41962256d1fb7eb6d9e41ac70f5e52fcda0d63
leungmanhin <manhinleung@gmail.com>
2020-04-20 14:39:41 +0800

    Parse the herb file

--- tcmid.py
+++ tcmid.py
@@ -16,11 +16,13 @@ tcmid_network = "ingredient_targets_disease_drug-TCMID.v2.03.rar"
 tcmid_gnsp = "Ingredient_MS-TCMID.v2.01.rar"
 tcmid_spectrum = "Herb_MS-TCMID.v2.01.rar"
 tcmid_source_rars = [
-  tcmid_prescription,
+  # Need to know the properties of the herb first, so tcmid_herb
+  # should be processed before tcmid_prescription
   tcmid_herb,
-  tcmid_network,
-  tcmid_gnsp,
-  tcmid_spectrum
+#  tcmid_prescription,
+#  tcmid_network,
+#  tcmid_gnsp,
+#  tcmid_spectrum
 ]
 tcmid_base_url = "http://119.3.41.228:8000/static/download/"
 
@@ -28,6 +30,7 @@ if os.path.exists(os.path.join(os.getcwd(), output_file)):
   os.remove(output_file)
 
 def evalink(pred, node_type1, node_type2, node1, node2):
+  print("--- Creating EvaluationLink with:\npredicate = {}\nnode1 = {}\nnode2 = {}\n".format(pred, node1, node2))
   out_fp.write("(EvaluationLink\n")
   out_fp.write("\t(PredicateNode \"" + pred + "\")\n")
   out_fp.write("\t(ListLink\n")
@@ -42,6 +45,13 @@ def memblink(node1, node2):
   out_fp.write("\t(ConceptNode \"" + node2 + "\")\n")
   out_fp.write(")\n")
 
+def is_available(entry):
+  return False if entry.strip() == "" or entry.strip().lower() == "na" else True
+
+# ----------
+# Keep a record of which part of a herb would be used in a formula
+herb_part_dict = {}
+
 out_fp = open(output_file, "a", encoding='utf8')
 
 for rar_name in tcmid_source_rars:
@@ -53,5 +63,33 @@ for rar_name in tcmid_source_rars:
 
   rar_file = wget.download(tcmid_base_url + rar_name, "raw_data")
   print("\nFile downloaded: {}".format(rar_file))
+
+  with rarfile.RarFile(rar_file) as rf:
+    # There should only be one file per RAR file
+    # Decode using UTF-8 for the Chinese characters
+    lines = rf.read(rf.infolist()[0]).decode("utf-8").split("\n")
+
+    if rar_file.endswith(tcmid_herb):
+      # Skip the first line (columns) in this file
+      for line in lines[1:]:
+        print("--- Reading line: " + line)
+        if is_available(line):
+          contents = line.split("\t")
+          pinyin_name = contents[0]
+          english_name = contents[2]
+          properties = [x.lower().strip() for x in contents[4].split(",")]
+          meridians = [x.lower().strip() for x in contents[5].split(",")]
+          use_part = contents[6]
+          if is_available(pinyin_name) and is_available(english_name):
+            evalink("has_name", "ConceptNode", "ConceptNode", pinyin_name, english_name)
+          if is_available(pinyin_name) and is_available(use_part):
+            herb_part_dict[pinyin_name] = use_part
+            evalink("has_part", "ConceptNode", "ConceptNode", pinyin_name, "{} {}".format(pinyin_name, use_part))
+          for prop in properties:
+            if is_available(pinyin_name) and is_available(prop):
+              evalink("has_property", "ConceptNode", "ConceptNode", pinyin_name, "TCM:" + prop)
+          for meri in meridians:
+            if is_available(pinyin_name) and is_available(meri):
+              evalink("targets_meridian", "ConceptNode", "ConceptNode", pinyin_name, "TCM:" + meri)
 
 out_fp.close()
