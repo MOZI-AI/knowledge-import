35ba38d45cd513d023b38c2bf2b075fb5ebfde64
leungmanhin <manhinleung@gmail.com>
2020-04-20 14:38:10 +0800

    Fix file outputting issue

--- tcmid.py
+++ tcmid.py
@@ -9,6 +9,7 @@ from datetime import date
 
 rarfile.UNRAR_TOOL = "unrar"
 
+output_file = "dataset/tcmid_{}.scm".format(str(date.today()))
 tcmid_prescription = "prescription-TCMID.v2.01.rar"
 tcmid_herb = "herb-TCMID.v2.01.rar"
 tcmid_network = "ingredient_targets_disease_drug-TCMID.v2.03.rar"
@@ -19,12 +20,9 @@ tcmid_source_rars = [
   tcmid_herb,
   tcmid_network,
   tcmid_gnsp,
-#  tcmid_spectrum
+  tcmid_spectrum
 ]
 tcmid_base_url = "http://119.3.41.228:8000/static/download/"
-
-output_file = "dataset/tcmid_{}.scm".format(str(date.today()))
-out_fp = open(output_file, "a", encoding='utf8')
 
 if os.path.exists(os.path.join(os.getcwd(), output_file)):
   os.remove(output_file)
@@ -43,6 +41,8 @@ def memblink(node1, node2):
   out_fp.write("\t(ConceptNode \"" + node1 + "\")\n")
   out_fp.write("\t(ConceptNode \"" + node2 + "\")\n")
   out_fp.write(")\n")
+
+out_fp = open(output_file, "a", encoding='utf8')
 
 for rar_name in tcmid_source_rars:
   rar_path = "raw_data/{}".format(rar_name)
