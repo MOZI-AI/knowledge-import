9b4b4c996867f29259db37d39611b2109e0cc66f
Abdulrahman Semrie <hsamireh@gmail.com>
2020-11-24 18:29:53 +0300

    Include state and pre-treatment variables in patient data importation

--- cancer/patient_outcome.py
+++ cancer/patient_outcome.py
@@ -2,31 +2,33 @@ __author__ = 'Abdulrahman Semrie<hsamireh@gmail.com>'
 
 import pandas as pd
 import argparse
-import os
-from atomwrappers import *
-from zipfile import ZipFile
 from io import BytesIO
+from zipfile import ZipFile
+
+from atomwrappers import *
 from pharmagkb import build_request
 
 gene_dict = {"p53_mutation": "TP53",  "PTEN_pos": "PTEN", "cytokeratin5_pos": "CK5"}
-ihc_dict = {"ER_preTrt": "ER_status", "Erbeta_preTrt": "Erbeta_status", "PR_preTrt": "PR_status", "HER2_preTrt": "HER2_status"}
-misc_dict = {"preTrt_lymph_node_status": "pre_node_status", "postTrt_lymph_node_status": "post_node_status", "tumor_stage_preTrt": "pre_tumor_stage", "tumor_stage_postTrt": "post_tumor_stage",
-             "clinical_AJCC_stage": "cancer_stage"}
+bool_vals = ["ER_preTrt", "ER_expr_preTrt", "ESR1_preTrt", "ERbb2_preTrt", "Erbeta_preTrt", "ERBB2_CPN_amplified", "PR_preTrt", "PR_expr_preTrt" ,"HER2_preTrt", "HER2_expr_preTrt", "p53_mutation", "p53", "PIK3CA_mutation", "PTEN_pos" "PTEN_mutation", "cytokeratin5_pos" ,"family_history"]
 
-def create_ihc_marker_ln(patient, marker, value, fp):
+non_bool_vals = ["biopsy_preTreat", "tumor_size_cm_preTrt_preSurgery", "tumor_size_cm_secondAxis_preTrt_preSurgery", "tumor_size_cm_preTrt_preSurgeryMin", "clinical_AJCC_stage", "clinical_AJCC_stageRangeMin", "clinical_AJCC_stageRangeMax", "preTrt_lymph_node_status", "preTrt_totalLymphNodes", "preTrt_numPosLymphNodes", "preTrt_numPosLymphNodesRemoved", "preTrt_posDichLymphNodes", "notthingham_prognostic_index", "tumor_stage_preTrt", "tumor_stage_preTrtMin", "tumor_stage_preTrtMax", "pam50", "hist_grade", "hist_gradeMax", "hist_gradeMin", "nuclear_grade_preTrt", "path_diagnosis", "path", "chemosensitivity_prediction", "metastasis_stage_preTrt", "ER_percentage_preTrt", "ER_fmolmg_preTrt", "PR_percentage_preTrt", "PR_fmolmg_preTrt", "HER2_IHC_score_preTrt", "HER2_fish_cont_score_preTrt", "HER2_SNP6_state", "p53_mutation_type", "ploidy", "top2atri_preTrt", "topoihc_preTrt", "S_phase", "DNA_index"]
+
+def create_state_ln(patient, marker, value, fp):
     """
     Creates & writes EvaluationLinks for patient ihc biomarker status
        eg.  (EvaluationLink (stv 1 1) (Predicate "ER_status")
                     (ListLink (Concept "1234") (Concept "positive")))
     """
     if not pd.isna(value):
-        marker_name = ihc_dict[marker]
-        if value == 0:
-            eval_ln = CEvaluationLink(CPredicateNode(marker_name), CListLink(patient, CConceptNode("negative")), stv=CStv(1.0, 1.0))
-        else:
-            eval_ln = CEvaluationLink(CPredicateNode(marker_name), CListLink(patient, CConceptNode("positive")), stv=CStv(1.0, 1.0))
-
-        fp.write(eval_ln.recursive_print() + "\n")
+        if marker in bool_vals:
+            if value == 0:
+                eval_ln = CEvaluationLink(CPredicateNode(marker), patient, stv=CStv(0.0, 1.0))
+            else:
+                eval_ln = CEvaluationLink(CPredicateNode(marker), patient, stv=CStv(1.0, 1.0))
+            fp.write(eval_ln.recursive_print() + "\n")
+        elif marker in non_bool_vals:
+            eval_ln = CEvaluationLink(CPredicateNode(marker), CListLink(patient, CConceptNode(str(value))), stv=CStv(1.0, 1.0))
+            fp.write(eval_ln.recursive_print() + "\n")
 
 
 def create_mutation_ln(patient, mut, value, fp):
@@ -76,7 +78,7 @@ def create_treatment_ln(patient, treatment, value, drugs_df, fp):
                     chebi = [x for x in cross_ref if x.startswith("ChEBI")]
                     if len(chebi) > 0: #look for a chebi id first
                         chebi_id = (chebi[0]).split(":")[-1]
-                        eval_ln = CEvaluationLink(CPredicateNode("has_treatment"), CListLink(patient, CChebiNode("ChEBI:" + chebi_id)),
+                        eval_ln = CEvaluationLink(CPredicateNode("has_treatment"), CListLink(patient, ChebiNode("ChEBI:" + chebi_id)),
                                               stv=CStv(1.0, 1.0))
                         fp.write(eval_ln.recursive_print() + "\n")
                     else: #look for pubchem
@@ -84,17 +86,12 @@ def create_treatment_ln(patient, treatment, value, drugs_df, fp):
                         if len(pubchem) > 0:
                             pubchem_id = (pubchem[0]).split(":")[-1]
                             eval_ln = CEvaluationLink(CPredicateNode("has_treatment"),
-                                      CListLink(patient, CPubchemNode("PubChem:" + pubchem_id)),
+                                      CListLink(patient, PubchemNode("PubChem:" + pubchem_id)),
                                       stv=CStv(1.0, 1.0))
                             fp.write(eval_ln.recursive_print() + "\n")
 
             except IndexError:
                 pass
-
-def create_misc_ln(patient, name, value, fp):
-    if not pd.isna(value):
-        eval_ln = CEvaluationLink(CPredicateNode(misc_dict[name]), CListLink(patient, CConceptNode(value)), stv=CStv(1.0, 1.0))
-        fp.write(eval_ln.recursive_print() + "\n")
 
 
 def import_info(df, drugs_df, fp):
@@ -104,24 +101,20 @@ def import_info(df, drugs_df, fp):
     outcome_cols = ['OS', 'DFS', 'RFS', 'metastasis', 'pCR']
     treatment_cols = df.loc[:, 'radiotherapyClass':'neoadjuvant_or_adjuvant'].columns.tolist()
     treatment_cols.append('surgery_type')
-    misc_status_col = ["preTrt_lymph_node_status", "postTrt_lymph_node_status", "clinical_AJCC_stage", "tumor_stage_preTrt", "tumor_stage_postTrt"]
 
     for i in range(df.shape[0]):
         patient = CConceptNode(str(df.iloc[i]["patient_ID"]))
-        for marker in ihc_marker_cols:
-            create_ihc_marker_ln(patient, marker, df.iloc[i][marker], fp)
+        for marker in bool_vals:
+            create_state_ln(patient, marker, df.iloc[i][marker], fp)
 
-        for mutation in mutation_cols:
-            create_mutation_ln(patient, mutation, df.iloc[i][mutation], fp)
+        for mutation in non_bool_vals:
+            create_state_ln(patient, mutation, df.iloc[i][mutation], fp)
 
         for outcome in outcome_cols:
             create_outcome_ln(patient, outcome, df.iloc[i][outcome], fp)
 
         for treatment in treatment_cols:
             create_treatment_ln(patient, treatment, df.iloc[i][treatment], drugs_df, fp)
-
-        for ns in misc_status_col:
-            create_misc_ln(patient, ns, df.iloc[i][ns], fp)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="convert clinical trial patient data to atomese")
